0997d755e5b73d591492778f190e39b70ad54ec8
FSP-1 <f.s.p282002@gmail.com>
2022-05-01 12:52:27 +0200

    x

--- software/scripts/install_clientNFS.sh
+++ software/scripts/install_clientNFS.sh
@@ -6,10 +6,10 @@
 
 apt-get install nfs-common -y
 #
-mount 172.31.90.154:/Practica_FSP/software/ /Practica_FSP/software/
+mount 172.31.92.138:/Practica_FSP/software/ /Practica_FSP/software/
 #
 
- sed -i "/LABEL=cloudimg-rootfs/a  172.31.90.154:/Practica_FSP/software/Pagina-Web/src /Practica_FSP/software/Pagina-Web/scr  nfs auto,nofail,noatime,nolock,intr,tcp,actimeo=1800 0 0" /etc/fstab
+ sed -i "/LABEL=cloudimg-rootfs/a 172.31.92.138:/Practica_FSP/software/Pagina-Web/ /Practica_FSP/software/Pagina-Web/  nfs auto,nofail,noatime,nolock,intr,tcp,actimeo=1800 0 0" /etc/fstab
 
 #
 systemctl restart nfs-client.target
