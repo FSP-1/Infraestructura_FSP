d7ae6d7d7b06a9d56908666733bbdc790f0b2fe1
FSP-1 <f.s.p282002@gmail.com>
2022-05-06 09:36:48 +0200

    Separo mysql del docker-compose

--- infractrutura/roles/tasks/06_Create_ip_elastica.sh
+++ infractrutura/roles/tasks/06_Create_ip_elastica.sh
@@ -11,7 +11,7 @@ INSTANCE_NAME_WWW1=Ash-Twin
 INSTANCE_NAME_WWW2=Ember_Twin
 INSTANCE_NAME_BALANCER=Balancer
 INSTANCE_NAME_NAGIOS=Nagios_server
-INSTANCE_NAME_ANSIBLE=Ansible
+INSTANCE_NAME_MYSQL=Mysql
 
 # Obtenemos el Id de la instancia a partir de su nombre
 INSTANCE_ID=$(aws ec2 describe-instances \
@@ -39,8 +39,8 @@ INSTANCE_ID5=$(aws ec2 describe-instances \
             --query "Reservations[*].Instances[*].InstanceId" \
             --output text)
             
-INSTANCE_ID6=$(aws ec2 describe-instances \
-            --filters "Name=tag:Name,Values=$INSTANCE_NAME_ANSIBLE" \
+INSTANCE_ID7=$(aws ec2 describe-instances \
+            --filters "Name=tag:Name,Values=$INSTANCE_NAME_MYSQL" \
                       "Name=instance-state-name,Values=running" \
             --query "Reservations[*].Instances[*].InstanceId" \
             --output text)
@@ -51,11 +51,11 @@ ELASTIC_IP=$(aws ec2 allocate-address --query PublicIp --output text)
 ELASTIC_IP2=$(aws ec2 allocate-address --query PublicIp --output text)
 ELASTIC_IP3=$(aws ec2 allocate-address --query PublicIp --output text)
 ELASTIC_IP5=$(aws ec2 allocate-address --query PublicIp --output text)
-ELASTIC_IP6=$(aws ec2 allocate-address --query PublicIp --output text)
+ELASTIC_IP7=$(aws ec2 allocate-address --query PublicIp --output text)
 
 # Asociamos la IP elástica a la instancia del balanceador
 aws ec2 associate-address --instance-id $INSTANCE_ID --public-ip $ELASTIC_IP
 aws ec2 associate-address --instance-id $INSTANCE_ID2 --public-ip $ELASTIC_IP2
 aws ec2 associate-address --instance-id $INSTANCE_ID3 --public-ip $ELASTIC_IP3
 aws ec2 associate-address --instance-id $INSTANCE_ID5 --public-ip $ELASTIC_IP5
-aws ec2 associate-address --instance-id $INSTANCE_ID6 --public-ip $ELASTIC_IP6
+aws ec2 associate-address --instance-id $INSTANCE_ID7 --public-ip $ELASTIC_IP7
