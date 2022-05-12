7cb6f8b7ca755bebd7e1b68383ba5dfa4571ac19
FSP-1 <f.s.p282002@gmail.com>
2022-05-12 16:31:06 +0200

    y

--- software/Pagina-Web/HTML/src/config.php
+++ software/Pagina-Web/HTML/src/config.php
@@ -1,10 +1,6 @@
 <?php
 
-<<<<<<< HEAD
 define('DB_HOST', '172.31.87.187');
-=======
-define('DB_HOST', '54.161.160.248');
->>>>>>> b8eefb2df36e2950d71f2e88a547e0d6dbab7424
 define('DB_NAME', 'Franco_db');
 define('DB_USER', 'Franco');
 define('DB_PASSWORD', '11Ww222Ee');
