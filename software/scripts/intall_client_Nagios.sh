d50d9bc0f2056e905b1620ab87d7936aa3b55d3b
FSP-1 <f.s.p282002@gmail.com>
2022-05-13 07:14:33 +0000

    ws

--- software/scripts/intall_client_Nagios.sh
+++ software/scripts/intall_client_Nagios.sh
@@ -5,8 +5,7 @@ apt update
 apt install libmcrypt-dev git autoconf make libssl-dev bc gawk dc build-essential snmp libnet-snmp-perl gettext libldap2-dev smbclient fping default-libmysqlclient-dev -y 
 
 # Instalamos los plugins nagios para que nos permita monitorizar los parámetros de las máquinas remotas.
-useradd nagios
-groupadd nagios
+
 cd /tmp/
 wget https://github.com/nagios-plugins/nagios-plugins/archive/release-2.2.1.tar.gz
 tar zxvf release-2.2.1.tar.gz 
@@ -27,6 +26,7 @@ cd nrpe
 autoconf 
 ./configure
 make all
+make install-groups-users
 make install
 make install-plugin
 make install-daemon
@@ -46,11 +46,10 @@ systemctl start nrpe
 # Cambiamos el fichero nrpe.cfg para permitir el acceso al servidor Nagios añadiendo su ip y los softwares o hardwares que vamos a chequear
 cd /tmp/
 git clone https://github.com/justintime/nagios-plugins.git
-cd nagios-plugins
 mv nagios-plugins/check_mem/check_mem.pl /usr/local/nagios/libexec
 chown nagios.nagios /usr/local/nagios/libexec/check_mem.pl 
-chmod +x /usr/local/nagios/nagios/libexec/check_mem.pl
-
+chmod +x /usr/local/nagios/libexec/check_mem.pl
+cd /home/ubuntu/Practica_FSP/software/scripts
 cp ../conf/nrpe.cfg /usr/local/nagios/etc/nrpe.cfg
 
 # Reiniciamos el servicio para que los cambios se apliquen
