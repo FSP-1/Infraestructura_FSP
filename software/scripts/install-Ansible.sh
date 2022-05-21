aff6a4b06cc136b75a6d86aac98e9ddb4016b77a
FSP-1 <f.s.p282002@gmail.com>
2022-05-21 19:40:28 +0200

    Amazon automatizado

--- software/scripts/install-Ansible.sh
+++ software/scripts/install-Ansible.sh
@@ -18,16 +18,14 @@ set -x
 # Instalamos Boto Framework - AWS SDK para que ansible pueda llegar a AWS usando boto SDK
  pip install boto boto3
  apt-get install python3-boto -y
-exit;
 #
 apt install zip -y
 cd /tmp/
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 unzip awscliv2.zip
 
- ./aws/install 
+ ./aws/install
 aws --version
 
 rm /etc/localtime
 ln -s /usr/share/zoneinfo/Europe/Madrid /etc/localtime	
-ssh-keygen
